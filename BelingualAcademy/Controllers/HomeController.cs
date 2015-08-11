c3120a3a11ecaf56fb9b4442f1d104b8fdce65f6
rschnierle <jose.schnierle@softtek.com>
2015-08-11 12:32:38 -0700

    Initial Project creation

---
+++ BelingualAcademy/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BelingualAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BelingualAcademy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var cursos = db.Courses.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BelingualAcademy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BelingualAcademy.Startup))]
+namespace BelingualAcademy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
